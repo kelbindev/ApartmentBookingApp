c750353d6cc6e4af0a33a2d00beb7b6bfa836de8
splc2068 <kelbin@shimano.com.sg>
2023-08-15 23:15:57 +0800

    Fix class name

--- Domain/Apartments/ApartmentErrors.cs
+++ Domain/Apartments/ApartmentErrors.cs
@@ -2,7 +2,7 @@
 
 namespace Domain.Apartments;
 
-public static class ApartmentsError
+public static class ApartmentErrors
 {
     public static Error NotFound = new("Apartment.Found", "the apartment with specific identifier was not found");
 }
